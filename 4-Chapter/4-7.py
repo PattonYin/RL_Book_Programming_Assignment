a568e2aa427e7225477574b25303eec7ac98da0e
PattonYin <pyin@wesleyan.edu>
2025-05-31 15:51:38 +0800

    updated the MDP code

--- 4-Chapter/4-7.py
+++ 4-Chapter/4-7.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import poisson
+from collections import defaultdict
 
 # Constants
 MAX_CARS = 20
@@ -21,10 +22,30 @@ class JackCarRentalEnv:
     def __init__(self):
         self.states = [(i, j) for i in range(MAX_CARS + 1) for j in range(MAX_CARS + 1)]
         self.actions = list(range(-max_move, max_move + 1)) # action > 0 means move from first location to second location and vice versa
-        
+
+        self.state_values = defaultdict(float)
+        self.initialize_state_values()
+
+        self.policy = defaultdict(int)
+        self.initialize_policy()
+
+    def initialize_state_values(self):
+        for state in self.states:
+            self.state_values[state] = 0
+
+    def initialize_policy(self):
+        for state in self.states:
+            self.policy[state] = 0
+
     def constrain_move(self, state, action):
-        
-        return
+        if action > 0: 
+            if state[0] - action < 0:
+                action = state[0]
+        else if action < 0:
+            if state[1] + action < 0:
+                action = -state[1]
+
+        return action
         
     def constrain_return(self, state):
         if state[0] > MAX_CARS:
@@ -48,8 +69,8 @@ class JackCarRentalEnv:
         
         return prob_1_return * prob_2_return * prob_1_request * prob_2_request
 
-    def compute_reward(self, actual_cars_requested, cars_moved):
-        return RENTAL_REWARD * actual_cars_requested[0] + RENTAL_REWARD * actual_cars_requested[1] - MOVE_COST * abs(actual_cars_requested[0] - cars_requested[0]) - MOVE_COST * abs(actual_cars_requested[1] - cars_requested[1])
+    def compute_reward(self, actual_cars_moved, actual_cars_requested):
+        return RENTAL_REWARD * actual_cars_requested[0] + RENTAL_REWARD * actual_cars_requested[1] - MOVE_COST * actual_cars_moved
 
     def transition_prob_and_reward(self, state, action):
         """ compute the transition probability and reward
@@ -57,16 +78,16 @@ class JackCarRentalEnv:
             state: (n_car_1, n_car_2)
             action: (n_move, target_loc)
         Output:
-            dict: 
-                key: next_state
-                value: (transition_prob, reward)
+            next_state
+            transition_prob
+            reward
         """
         cars_returned = np.random.poisson(RETURNS_FIRST_LOC), np.random.poisson(RETURNS_SECOND_LOC)
         cars_requested = np.random.poisson(RENTAL_REQUEST_FIRST_LOC), np.random.poisson(RENTAL_REQUEST_SECOND_LOC)   
         
         # after action
-        next_state_1 = (state[0] + action, state[1] - action)
-        next_state_1 = self.constrain_state(next_state_1)
+        actual_action = self.constrain_move(state, action)
+        next_state_1 = (state[0] + actual_action, state[1] - actual_action)
         
         # after return
         next_state_2 = (next_state_1[0] + cars_returned[0], next_state_1[1] + cars_returned[1])
@@ -76,11 +97,30 @@ class JackCarRentalEnv:
         actual_cars_requested = self.constrain_rental(next_state_2, cars_requested)       
         next_state_3 = (next_state_2[0] - actual_cars_requested[0], next_state_2[1] - actual_cars_requested[1])
 
-        
+        transition_prob = self.compute_transition_prob(cars_returned, cars_requested)
+        reward = self.compute_reward(actual_action, actual_cars_requested)
+
+        return next_state_3, transition_prob, reward
+            
         # Implementation question, do I need to consider the return time for each individual car? Answer: No, the Poisson distribution of cars in and out took care of it.
         # However, in this case, doesn't it mean that the car rental doesn't affect the rewards? Since the return probability is deterministic and doesn't depend on the state. 
         # Nevermind, I just realized that the reward are dependent on the car rental request, which does depend on the state.
         
-        
+    def policy_evaluation(self):
+        threshold = 0.1
+        delta = 100
+        new_state_values = defaultdict(float)
+        while delta > threshold:
+            for state in self.states:
+                new_state_values[state] = self.state_values[state] + DISCOUNT * sum(self.transition_prob_and_reward(state, self.policy[state])[1] * self.state_values[self.transition_prob_and_reward(state, self.policy[state])[0]])
+
+    # How am I supposed to compute the state transition probability? 
+    # How about this, for all actions, I compute a probability for each action on each state, and aggregate them together using average? Because the next state definitely depends on the action / policy, it is impossible to skip that.
+    #  
+        return
+
+    def policy_improvement(self):
+        return
+            
         
         
